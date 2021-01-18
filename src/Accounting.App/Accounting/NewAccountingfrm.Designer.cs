cc0ec59bbfd1d8bee2d310be332129251ef884b1
AmirAliShokri <amirali.shokri@gmail.com>
2021-01-18 19:01:20 +0330

    Compelete Accounting Form

--- src/Accounting.App/Accounting/NewAccountingfrm.Designer.cs
+++ src/Accounting.App/Accounting/NewAccountingfrm.Designer.cs
@@ -29,14 +29,30 @@ namespace Accounting.App
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NewAccountingfrm));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.txtFilter = new System.Windows.Forms.TextBox();
             this.dgvCustomer = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.CustomerID = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCustomer = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.rbtncost = new System.Windows.Forms.RadioButton();
+            this.rbtnInCome = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.btncancel = new System.Windows.Forms.Button();
+            this.rangeValidator1 = new ValidationComponents.RangeValidator(this.components);
+            this.requiredFieldValidator1 = new ValidationComponents.RequiredFieldValidator(this.components);
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvCustomer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -56,7 +72,7 @@ namespace Accounting.App
             this.txtFilter.Location = new System.Drawing.Point(49, 19);
             this.txtFilter.Name = "txtFilter";
             this.txtFilter.Size = new System.Drawing.Size(167, 23);
-            this.txtFilter.TabIndex = 0;
+            this.txtFilter.TabIndex = 7;
             this.txtFilter.TextChanged += new System.EventHandler(this.txtFilter_TextChanged);
             // 
             // dgvCustomer
@@ -72,7 +88,8 @@ namespace Accounting.App
             this.dgvCustomer.Name = "dgvCustomer";
             this.dgvCustomer.ReadOnly = true;
             this.dgvCustomer.Size = new System.Drawing.Size(210, 299);
-            this.dgvCustomer.TabIndex = 1;
+            this.dgvCustomer.TabIndex = 8;
+            this.dgvCustomer.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvCustomer_CellClick);
             // 
             // label1
             // 
@@ -98,12 +115,146 @@ namespace Accounting.App
             this.Column1.Name = "Column1";
             this.Column1.ReadOnly = true;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(240, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Customer";
+            // 
+            // txtCustomer
+            // 
+            this.txtCustomer.Location = new System.Drawing.Point(310, 6);
+            this.txtCustomer.Name = "txtCustomer";
+            this.txtCustomer.ReadOnly = true;
+            this.txtCustomer.Size = new System.Drawing.Size(274, 23);
+            this.txtCustomer.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(240, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Accounting Type";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(240, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Amount";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(234, 112);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Description";
+            // 
+            // rbtncost
+            // 
+            this.rbtncost.AutoSize = true;
+            this.rbtncost.Location = new System.Drawing.Point(464, 36);
+            this.rbtncost.Name = "rbtncost";
+            this.rbtncost.Size = new System.Drawing.Size(52, 21);
+            this.rbtncost.TabIndex = 2;
+            this.rbtncost.Text = "Cost";
+            this.rbtncost.UseVisualStyleBackColor = true;
+            // 
+            // rbtnInCome
+            // 
+            this.rbtnInCome.AutoSize = true;
+            this.rbtnInCome.Checked = true;
+            this.rbtnInCome.Location = new System.Drawing.Point(373, 36);
+            this.rbtnInCome.Name = "rbtnInCome";
+            this.rbtnInCome.Size = new System.Drawing.Size(71, 21);
+            this.rbtnInCome.TabIndex = 1;
+            this.rbtnInCome.TabStop = true;
+            this.rbtnInCome.Text = "InCome";
+            this.rbtnInCome.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(310, 109);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(274, 209);
+            this.textBox1.TabIndex = 4;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(310, 71);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            999999999,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(274, 23);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // btnsave
+            // 
+            this.btnsave.Location = new System.Drawing.Point(310, 330);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(142, 23);
+            this.btnsave.TabIndex = 5;
+            this.btnsave.Text = "Save";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // btncancel
+            // 
+            this.btncancel.Location = new System.Drawing.Point(452, 330);
+            this.btncancel.Name = "btncancel";
+            this.btncancel.Size = new System.Drawing.Size(132, 23);
+            this.btncancel.TabIndex = 6;
+            this.btncancel.Text = "Cancel";
+            this.btncancel.UseVisualStyleBackColor = true;
+            this.btncancel.Click += new System.EventHandler(this.btncancel_Click);
+            // 
+            // rangeValidator1
+            // 
+            this.rangeValidator1.CancelFocusChangeWhenInvalid = false;
+            this.rangeValidator1.ControlToValidate = this.numericUpDown1;
+            this.rangeValidator1.ErrorMessage = "Please Use a True Value";
+            this.rangeValidator1.Icon = ((System.Drawing.Icon)(resources.GetObject("rangeValidator1.Icon")));
+            this.rangeValidator1.MaximumValue = "999999999";
+            this.rangeValidator1.MinimumValue = "1";
+            this.rangeValidator1.Type = ValidationComponents.ValidationDataType.Integer;
+            // 
+            // requiredFieldValidator1
+            // 
+            this.requiredFieldValidator1.CancelFocusChangeWhenInvalid = false;
+            this.requiredFieldValidator1.ControlToValidate = this.txtCustomer;
+            this.requiredFieldValidator1.ErrorMessage = "Please Select a Customer";
+            this.requiredFieldValidator1.Icon = ((System.Drawing.Icon)(resources.GetObject("requiredFieldValidator1.Icon")));
+            // 
             // NewAccountingfrm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(596, 365);
+            this.Controls.Add(this.btncancel);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.rbtnInCome);
+            this.Controls.Add(this.rbtncost);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCustomer);
             this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
             this.Font = new System.Drawing.Font("Microsoft YaHei", 9F);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
@@ -114,7 +265,9 @@ namespace Accounting.App
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvCustomer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -126,5 +279,18 @@ namespace Accounting.App
         private System.Windows.Forms.DataGridViewTextBoxColumn CustomerID;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.TextBox txtFilter;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCustomer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton rbtncost;
+        private System.Windows.Forms.RadioButton rbtnInCome;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.Button btncancel;
+        private ValidationComponents.RangeValidator rangeValidator1;
+        private ValidationComponents.RequiredFieldValidator requiredFieldValidator1;
     }
 }
--- src/Accounting.App/Accounting/NewAccountingfrm.cs
+++ src/Accounting.App/Accounting/NewAccountingfrm.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Accounting.DataLayer.Context;
+using Accounting.DataLayer;
+using ValidationComponents;
+using Accounting = Accounting.DataLayer.Accounting;
 
 namespace Accounting.App
 {
@@ -28,6 +31,34 @@ namespace Accounting.App
         private void txtFilter_TextChanged(object sender, EventArgs e)
         {
             dgvCustomer.DataSource = db.CustomerRepository.GetCustomersByFilter(txtFilter.Text);
+        }
+
+        private void dgvCustomer_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtCustomer.Text = dgvCustomer.CurrentRow.Cells[1].Value.ToString();
+        }
+
+        private void btncancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+            if (BaseValidator.IsFormValid(this.components))
+            {
+                DataLayer.Accounting accounting = new DataLayer.Accounting()
+                {
+                    CustomerID = int.Parse(dgvCustomer.CurrentRow.Cells[0].Value.ToString()),
+                    TypeID = byte.Parse(((rbtnInCome.Checked) ? 1 : 2).ToString()),
+                    Amount = int.Parse(numericUpDown1.Text.ToString()),
+                    DateTime = DateTime.Now,
+                    Description = textBox1.Text
+                };
+                db.BaseRepositoryAccounting.Insert(accounting);
+                db.Save();
+                DialogResult = DialogResult.OK;
+            }
         }
     }
 }
